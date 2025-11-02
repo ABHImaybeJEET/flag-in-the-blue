d36b5199d273f1ee9da44f336671282a602b90da
ayush <ayushatschool5@gmail.com>
2025-11-02 12:17:47 +0530

    feat: setup tailwind and add shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "./components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+            <main className="flex min-h-screen flex-col items-center justify-center p-4">
+                Flag in the Blue
+                <Button>DSCxOC</Button>
+            </main>
+        </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
