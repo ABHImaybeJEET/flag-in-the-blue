4abdfeff9eb6ad15598decd1e39b4ba5dcba9e63
ayush <ayushatschool5@gmail.com>
2025-11-04 22:43:43 +0530

    feat: add react-hot-toast for score submission feedback and update .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import OceanScene from "./components/OceanScene";
-import { Button } from "./components/ui/button";
-import { ThemeProvider } from "@/components/theme-provider";
-import HintBox from "./components/hints";
 import FinalAnswerPopup from "./components/finish";
-import CorrectAnswerPopup from "./components/correct";
-import WrongAnswerPopup from "./components/wrong";
+
+import OceanScene from "./components/OceanScene";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
     return (
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
             <main className="flex min-h-screen flex-col items-center justify-center">
                 <OceanScene />
-                <FinalAnswerPopup/>
+                <FinalAnswerPopup />
             </main>
         </ThemeProvider>
     );
--- src/firebase/scores.js
+++ src/firebase/scores.js
@@ -1,17 +1,20 @@
-import {updateDoc,doc, getFirestore} from 'firebase/firestore';
+import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase/firebase.js";
+import { toast } from "react-hot-toast";
 
-export async function submitScore(timeTaken:number,penalty:number){
-	const userId = localStorage.getItem('uid');
+export async function submitScore(timeTaken, penalty) {
+    const userId = localStorage.getItem("uid");
     if (!userId) {
-		throw new Error("User is not logged in!");
+        throw new Error("User is not logged in!");
     }
-	const totalScore = timeTaken+penalty;
-	try{
-		const scoresCollRef=doc(db,"users",userId);
-		await updateDoc(scoresCollRef,{totalScore:totalScore});
-		console.log("Score submitted successfully!");
-	}catch(err){
-		console.error("Error submitting TotalScore:",err);
-	}
+    const totalScore = timeTaken + penalty;
+    try {
+        const scoresCollRef = doc(db, "users", userId);
+        await updateDoc(scoresCollRef, { totalScore: totalScore });
+        console.log("Score submitted successfully!");
+        toast.success("Score submitted successfully!");
+    } catch (err) {
+        toast.error("Failed to submit score");
+        console.error("Error submitting score: ", err);
+    }
 }
